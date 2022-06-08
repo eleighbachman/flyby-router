b6c63fb56a99660d46eda007ac6697b494775dc4
Liz Hennessy <eleighhennessy@gmail.com>
2022-06-08 11:25:56 -0600

    feat(router): add router

--- subgraph-reviews/resolvers.js
+++ subgraph-reviews/resolvers.js
@@ -4,6 +4,22 @@ const resolvers = {
       return dataSources.reviewsAPI.getLatestReviews();
     }
   },
+  Review: {
+    location: ({locationId}) => {
+      return {id: locationId};
+    }
+  },
+  Location: {
+    __resolveReference: (location) => {
+      return location
+    },
+    overallRating: ({id}, _, {dataSources}) => {
+      return dataSources.reviewsAPI.getOverallRatingForLocation(id);
+    },
+    reviewsForLocation: ({id}, _, {dataSources}) => {
+      return dataSources.reviewsAPI.getReviewsForLocation(id);
+    }
+  },
   Mutation: {
     submitReview: (_, {locationReview}, {dataSources}) => {
       const newReview = dataSources.reviewsAPI.submitReviewForLocation(locationReview);
